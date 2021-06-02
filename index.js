5a1fc2e3c2288f7c87f878892ab05a39978c625c
aziz <azizsassu2015@gmail.com>
2021-06-02 21:12:19 +0500

    calculating

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const form = document.querySelector('form'),
 calc = document.querySelector('.calculator'),
 value = calc.querySelector('.value'),
 symbols = calc.querySelectorAll('.num')
+value.value = 0;
+
+form.onsubmit = calculate
+
+function calculate(params){
+    params.preventDefault()
+}
 
 symbols.forEach((btn)=>{
     btn.addEventListener('click', ()=>{
-        if(btn.innerHTML === 'c') value.value = '' 
+        if(value.value == '0') value.value = ''
+        if(btn.innerHTML === 'c') value.value = '0' 
+        else if(btn.innerHTML == '=') value.value = eval(value.value)
         else value.value += btn.innerHTML  
     })
-})
+}) 
 
 
 
